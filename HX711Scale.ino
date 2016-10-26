3d1733b29c641643b462e7af634d56293b1fd588
Serg <redetection@gmail.com>
2016-10-26 15:40:58 +0700

    send measurements over bluetooth low energy

--- HX711Scale.ino
+++ HX711Scale.ino
@@ -1,12 +1,21 @@
 #include <SevSeg.h>
 #include <HX711.h>
 #include <avr/sleep.h>
+#include <BTLE.h>
+#include <RF24.h>
 
 HX711 scale(7, 6);		// parameter "gain" is ommited; the default value 128 is used by the library
 SevSeg sevseg;
+RF24 radio(9,10);
+BTLE btle(&radio);
 unsigned long long lastChangedTime;
 float lastWeight;
 
+struct nrf_weight_data {
+  int16_t service_uuid;
+  int16_t value;
+};
+nrf_weight_data payload;
 
 void setup() {
   byte digitPins[] = {14, 17, 18, 8};
@@ -14,6 +23,8 @@ void setup() {
   sevseg.begin(COMMON_ANODE, 4, digitPins, segmentPins);
   sevseg.setBrightness(50);
   scale.read();
+  btle.begin("Scale"); //8 chars max
+  payload.service_uuid = 0x181D; // org.bluetooth.service.weight_scale
   scale.set_scale(27477.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();				        // reset the scale to 0
 //  Serial.begin(9600);
@@ -34,6 +45,11 @@ void loop() {
   float kg = scale.get_units(1);
 //  Serial.println(kg);
   sevseg.setNumber(kg, 1);
+
+  payload.value = kg/0.005;
+  btle.advertise(0x16, &payload, sizeof(payload));
+  btle.hopChannel();
+  
   unsigned long long t = millis() + 1000;
   while(t > millis()) {
     sevseg.refreshDisplay();
